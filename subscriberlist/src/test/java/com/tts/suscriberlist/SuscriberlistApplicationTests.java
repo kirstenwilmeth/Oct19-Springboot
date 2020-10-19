2e2afb94ed69fdcaca517e6f073fe39233f91650
Kirsten <>
2020-10-19 10:24:41 -0500

    init commit

---
+++ subscriberlist/src/test/java/com/tts/suscriberlist/SuscriberlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.suscriberlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuscriberlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
